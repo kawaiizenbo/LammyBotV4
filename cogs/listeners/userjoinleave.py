3ef0f3979be1e012333b49b6d6183c2e8d626bc4
kawaiizenbo <48113593+kawaiizenbo@users.noreply.github.com>
2022-02-14 10:09:05 -0700

    embed join/leave

--- cogs/listeners/userjoinleave.py
+++ cogs/listeners/userjoinleave.py
@@ -14,10 +14,18 @@ class UserJoinLeave(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         server = member.guild
-        await server.get_channel(cfg["WELCOME_GOODBYE_CHANNEL_ID"]).send(f"Welcome, `{member.name}`")
+        embed = discord.Embed(
+            color = 0xBF005F,
+            description = f"Welcome, {member.mention}"
+        )
+        await server.get_channel(cfg["WELCOME_GOODBYE_CHANNEL_ID"]).send(embed = embed)
         await member.add_roles(server.get_role(cfg["AUTOROLE_ID"]), reason="Autorole")
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         server = member.guild
-        await server.get_channel(cfg["WELCOME_GOODBYE_CHANNEL_ID"]).send(f"Goodbye, `{member.name}`")
+        embed = discord.Embed(
+            color = 0xBF005F,
+            description = f"Goodbye, {member.mention}"
+        )
+        await server.get_channel(cfg["WELCOME_GOODBYE_CHANNEL_ID"]).send(embed = embed)
